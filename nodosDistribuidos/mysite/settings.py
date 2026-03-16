46abea3acb199bd3ddb24d00383c2b1d065eabe6
steveen-u <s.restrepo3@utp.edu.co>
2026-03-16 17:54:18 -0500

    Feat: Integración real con backend, funcionalidad de ver detalles (Modal) y copiar al portapapeles

--- nodosDistribuidos/mysite/settings.py
+++ nodosDistribuidos/mysite/settings.py
@@ -77,23 +77,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
- #       'ENGINE': 'django.db.backends.mysql',
-  #      'NAME': 'nonstandardb', #Si jode por el nombre de la base de datos entonces es nonstandarDB
-   #     'USER': 'root',
-    #    'PASSWORD': '1234',
-     #   'PORT':'3306',
-      #  'HOST':'127.0.0.1',     
-   # }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nonstandardb', #Si jode por el nombre de la base de datos entonces es nonstandarDB
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'PORT':'3306',
+        'HOST':'127.0.0.1',     
     }
 }
+
 
 
 # Password validation
